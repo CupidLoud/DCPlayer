89e99a33ed9ec1c550020c6946f3def7c59a29e9
lq150924 <1515551264@qq.com>
2018-05-15 14:21:19 +0800

    Initial Commit

---
+++ DCPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DCPlayer
+//
+//  Created by JunWin on 2018/5/15.
+//  Copyright © 2018年 freeWorld2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
